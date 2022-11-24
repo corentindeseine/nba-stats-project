388fb51d1fe7b2a98166fd3f87990c388c9f02c2
Corentin Deseine <deseinecorentin@gmail.com>
2022-11-24 01:16:09 +0100

    good pour navbar et page games

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,35 @@
 require 'open-uri'
 
-# Team.destroy_all
+Game.destroy_all
+Team.destroy_all
 
-# url = 'https://www.balldontlie.io/api/v1/teams'
-# teams_serialized = URI.open(url).read
-# teams_data = JSON.parse(teams_serialized)['data']
+url = 'https://www.balldontlie.io/api/v1/teams'
+teams_serialized = URI.open(url).read
+teams_data = JSON.parse(teams_serialized)['data']
 
-# ## CREATE NBA TEAMS ##
+## CREATE NBA TEAMS ##
 
-# def url_image(name)
-#   "https://res.cloudinary.com/dj1mlcnes/image/upload/v1667254439/nba-project/#{name}.png"
-# end
+def url_image(name)
+  "https://res.cloudinary.com/dj1mlcnes/image/upload/v1667254439/nba-project/#{name}.png"
+end
 
-# teams_data.each do |team|
-#   Team.create!(
-#     name: team['full_name'],
-#     abbreviation: team['abbreviation'],
-#     conference: team['conference'],
-#     api_id: team['id']
-#   )
-# end
+teams_data.each do |team|
+  Team.create!(
+    name: team['full_name'],
+    abbreviation: team['abbreviation'],
+    conference: team['conference'],
+    api_id: team['id']
+  )
+end
 
-# Team.all.each do |team|
-#   team.logo.attach(io: URI.open(url_image(team.abbreviation)),
-#                    filename: "#{team.abbreviation}-logo.png",
-#                    content_type: 'image/png')
-#   team.save!
-# end
+Team.all.each do |team|
+  team.logo.attach(io: URI.open(url_image(team.abbreviation)),
+                   filename: "#{team.abbreviation}-logo.png",
+                   content_type: 'image/png')
+  team.save!
+end
 
 ## CREATE GAMES ##
-
-Game.destroy_all
 
 start_date = (Date.today - 24).strftime('%Y-%m-%d')
 end_date = Time.now.strftime('%Y-%m-%d')
