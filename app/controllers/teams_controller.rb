1b4466f8395f9eb646d1e25b9f85c69e032f89cc
Corentin Deseine <deseinecorentin@gmail.com>
2022-12-05 02:05:32 +0100

    player with 0 minutes are no more in team show

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,6 +6,6 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find(params[:id])
     @past_games = @team.past_games(@team)
-    @players = @team.sort_players(@team, :min)
+    @players = @team.sort_players(@team, :min).reject { |player| player.average(player.stats, player)[:min].zero? }
   end
 end
