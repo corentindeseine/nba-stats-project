bb24d485ef6c5f2cff6ac4f01169ff4aee966278
Corentin Deseine <deseinecorentin@gmail.com>
2022-12-06 18:13:55 +0100

    css card play sur team show + debut stat player sur modal game (team show)

--- app/models/player.rb
+++ app/models/player.rb
@@ -15,19 +15,24 @@ class Player < ApplicationRecord
       turnover: average_stat(stats, player, 'turnover'),
       fg3_made: average_stat(stats, player, 'fg3_made'),
       dreb: average_stat(stats, player, 'dreb'),
-      fg_pct: average_stat_pct(stats, player, 'fg_pct'),
-      fg3_pct: average_stat_pct(stats, player, 'fg3_pct'),
-      ft_pct: average_stat_pct(stats, player, 'ft_pct')
+      fg_pct: average_stat(stats, player, 'fg_pct'),
+      fg3_pct: average_stat(stats, player, 'fg3_pct'),
+      ft_pct: average_stat(stats, player, 'ft_pct'),
+      games_played: games_played(player)
     }
   end
 
   private
 
   def average_stat(stats, player, param)
-    (stats.reverse.first(10).inject(0) { |sum, stat| sum.zero? ? (sum + stat.public_send(param)) : (sum + stat.public_send(param) + 0.0) } / 10).round(1)
+    if games_played(player) != 0
+      (stats.reverse.first(10).inject(0) { |sum, stat| sum.zero? ? (sum + stat.public_send(param)) : (sum + stat.public_send(param) + 0.0) } / games_played(player)).round(1)
+    else
+      0
+    end
   end
 
-  def average_stat_pct(stats, player, param)
-    (stats.reverse.first(10).inject(0) { |sum, stat| sum + stat.public_send(param) } / player.stats.reverse.first(10).reject { |stat| stat.min.zero? }.length).round(1)
+  def games_played(player)
+    player.stats.reverse.first(10).reject { |stat| stat.min.zero? }.length
   end
 end
