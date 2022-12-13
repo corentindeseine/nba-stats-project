55d95197f656f4e617cfe3f979b7b12c1389fae6
Corentin Deseine <deseinecorentin@gmail.com>
2022-12-13 10:35:33 +0100

    add five game rating and comparaison to player card

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,34 +6,35 @@ class Player < ApplicationRecord
 
   def average(stats, player)
     {
-      min: average_stat(stats, player, 'min'),
-      pts: average_stat(stats, player, 'pts'),
-      reb: average_stat(stats, player, 'reb'),
-      ast: average_stat(stats, player, 'ast'),
-      blk: average_stat(stats, player, 'blk'),
-      stl: average_stat(stats, player, 'stl'),
-      turnover: average_stat(stats, player, 'turnover'),
-      fg3_made: average_stat(stats, player, 'fg3_made'),
-      dreb: average_stat(stats, player, 'dreb'),
-      fg_pct: average_stat(stats, player, 'fg_pct'),
-      fg3_pct: average_stat(stats, player, 'fg3_pct'),
-      ft_pct: average_stat(stats, player, 'ft_pct'),
-      games_played: games_played(player),
-      rating: average_stat(stats, player, 'rating')
+      min: average_stat(stats, player, 'min', 10),
+      pts: average_stat(stats, player, 'pts', 10),
+      reb: average_stat(stats, player, 'reb', 10),
+      ast: average_stat(stats, player, 'ast', 10),
+      blk: average_stat(stats, player, 'blk', 10),
+      stl: average_stat(stats, player, 'stl', 10),
+      turnover: average_stat(stats, player, 'turnover', 10),
+      fg3_made: average_stat(stats, player, 'fg3_made', 10),
+      dreb: average_stat(stats, player, 'dreb', 10),
+      fg_pct: average_stat(stats, player, 'fg_pct', 10),
+      fg3_pct: average_stat(stats, player, 'fg3_pct', 10),
+      ft_pct: average_stat(stats, player, 'ft_pct', 10),
+      games_played: games_played(player, 10),
+      rating: average_stat(stats, player, 'rating', 10),
+      rating_five_games: average_stat(stats, player, 'rating', 5)
     }
   end
 
   private
 
-  def average_stat(stats, player, param)
-    if games_played(player) != 0
-      (stats.reverse.first(10).inject(0) { |sum, stat| sum.zero? ? (sum + stat.public_send(param)) : (sum + stat.public_send(param) + 0.0) } / games_played(player)).round(1)
+  def average_stat(stats, player, param, games_number)
+    if games_played(player, games_number) != 0
+      (stats.reverse.first(games_number).inject(0) { |sum, stat| sum.zero? ? (sum + stat.public_send(param)) : (sum + stat.public_send(param) + 0.0) } / games_played(player, games_number)).round(1)
     else
       0
     end
   end
 
-  def games_played(player)
-    player.stats.reverse.first(10).reject { |stat| stat.min.zero? }.length
+  def games_played(player, games_number)
+    player.stats.reverse.first(games_number).reject { |stat| stat.min.zero? }.length
   end
 end
