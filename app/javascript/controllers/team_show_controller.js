42c4166764c19c1a58af2f688917444851b381e4
Corentin Deseine <deseinecorentin@gmail.com>
2022-12-16 22:45:11 +0100

    filter W/L games on team show done

--- app/javascript/controllers/team_show_controller.js
+++ app/javascript/controllers/team_show_controller.js
@@ -2,10 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 import modal_game_controller from "./modal_game_controller";
 
 export default class extends Controller {
-  static targets= ['modalGame','WinLoseMenu'];
+  static targets= ['modalGame','winLoseMenu','win','lose','gameCard'];
 
   connect() {
-    console.log('hello')
   }
 
   launchGame(event) {
@@ -17,9 +16,37 @@ export default class extends Controller {
     modalController.open();
   }
 
-  ShowWinLoseMenu(event){
-    console.log(event.currentTarget)
+  showWinLoseMenu(event){
     event.currentTarget.classList.toggle("rotate")
-    this.WinLoseMenuTarget.classList.toggle('d-none')
+    this.winLoseMenuTarget.classList.toggle('d-none')
+  }
+
+  sortByWin(event){
+    event.currentTarget.classList.toggle("selected-option")
+    this.loseTarget.classList.remove("selected-option")
+    this.gameCardTargets.forEach(e => {
+
+      if (e.dataset.teamName === e.dataset.homeName && parseInt(e.dataset.visitorScore) > parseInt(e.dataset.homeScore)) {
+        e.classList.toggle('d-none')
+      } else if ( e.dataset.teamName === e.dataset.visitorName && parseInt(e.dataset.visitorScore) < parseInt(e.dataset.homeScore)) {
+        e.classList.toggle('d-none')
+      } else {
+        e.classList.remove('d-none')
+      }
+    });
+  }
+
+  sortByLose(event){
+    event.currentTarget.classList.toggle("selected-option")
+    this.winTarget.classList.remove("selected-option")
+    this.gameCardTargets.forEach(e => {
+      if (e.dataset.teamName === e.dataset.homeName && parseInt(e.dataset.visitorScore) < parseInt(e.dataset.homeScore)) {
+        e.classList.toggle('d-none')
+      } else if ( e.dataset.teamName === e.dataset.visitorName && parseInt(e.dataset.visitorScore) > parseInt(e.dataset.homeScore)) {
+        e.classList.toggle('d-none')
+      } else {
+        e.classList.remove('d-none')
+      }
+    });
   }
 }
