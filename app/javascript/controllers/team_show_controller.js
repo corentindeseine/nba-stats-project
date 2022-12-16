56d3bfc8d6cee290ada16ea212397e1056f82962
Corentin Deseine <deseinecorentin@gmail.com>
2022-12-16 18:49:03 +0100

    chevron list front set

--- app/javascript/controllers/team_show_controller.js
+++ app/javascript/controllers/team_show_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 import modal_game_controller from "./modal_game_controller";
 
 export default class extends Controller {
-  static targets= ['modalGame'];
+  static targets= ['modalGame','WinLoseMenu'];
 
   connect() {
+    console.log('hello')
   }
 
   launchGame(event) {
@@ -14,5 +15,11 @@ export default class extends Controller {
     );
     modalController.setCoHostContent(event.currentTarget.dataset)
     modalController.open();
+  }
+
+  ShowWinLoseMenu(event){
+    console.log(event.currentTarget)
+    event.currentTarget.classList.toggle("rotate")
+    this.WinLoseMenuTarget.classList.toggle('d-none')
   }
 }
