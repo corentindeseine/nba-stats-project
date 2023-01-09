388806112055f95b924c97ee9a4189d7eaec41dc
Corentin Deseine <deseinecorentin@gmail.com>
2023-01-09 23:27:18 +0100

    push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
+
   def versus
     @players = []
     @search_player_one = params['search']
-    if @search_player_one.present?
-      @player_one = @search_player_one['player_one']
-      @player_two = @search_player_one['player_two']
-      @players.push(Player.find_by(last_name: @player_one), Player.find_by(last_name: @player_two))
-    end
+
+    return unless @search_player_one.present?
+
+    @player_one = @search_player_one['player_one']
+    @player_two = @search_player_one['player_two']
+    @players.push(Player.find_by(last_name: @player_one), Player.find_by(last_name: @player_two))
+  end
+
+  def favorite
+    @players = []
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/player-vs-player', to: 'pages#versus'
+  get '/favorite', to: 'pages#favorite'
   resources :teams, only: %i[index show]
   resources :games, only: %i[index show]
   resources :players, only: %i[index show]
