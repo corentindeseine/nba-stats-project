2c71688e91ea8abef98fdc25751518b810b16747
CorentinDeseine <deseinecorentin@gmail.com>
2023-11-07 17:57:15 +0100

    fix npm issues

--- app/javascript/controllers/home_animation_controller.js
+++ app/javascript/controllers/home_animation_controller.js
@@ -1,57 +1,61 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
- static targets = ['games','players','players_vs','teams']
+  static targets = ['games_reviews', 'players_stats', 'players_vs', 'teams', 'fantasy_league']
 
   connect() {
-    this.gamesTarget.classList.add('invisible');
-    this.gamesTarget.classList.remove('ball-hover');
+    this.games_reviewsTarget.classList.add('invisible');
+    this.games_reviewsTarget.classList.remove('ball-hover');
 
-    this.playersTarget.classList.add('invisible');
-    this.playersTarget.classList.remove('ball-hover');
+    this.players_statsTarget.classList.add('invisible');
+    this.players_statsTarget.classList.remove('ball-hover');
 
     this.players_vsTarget.classList.add('invisible');
     this.players_vsTarget.classList.remove('ball-hover');
 
     this.teamsTarget.classList.add('invisible');
     this.teamsTarget.classList.remove('ball-hover');
+
+    this.fantasy_leagueTarget.classList.add('invisible');
+    this.fantasy_leagueTarget.classList.remove('ball-hover');
   }
 
   mouseOver(event) {
-    if (event.currentTarget.innerHTML === 'Games') {
-      this.gamesTarget.classList.remove('invisible');
-      this.gamesTarget.classList.add('ball-hover');
-
-    } else if (event.currentTarget.innerHTML === 'Players'){
-      this.playersTarget.classList.remove('invisible');
-      this.playersTarget.classList.add('ball-hover');
-
-    } else if (event.currentTarget.innerHTML === 'Players VS'){
+    console.log('hello')
+    if (event.currentTarget.innerHTML === 'Games reviews') {
+      this.games_reviewsTarget.classList.remove('invisible');
+      this.games_reviewsTarget.classList.add('ball-hover');
+    } else if (event.currentTarget.innerHTML === 'Players stats') {
+      this.players_statsTarget.classList.remove('invisible');
+      this.players_statsTarget.classList.add('ball-hover');
+    } else if (event.currentTarget.innerHTML === 'Players VS') {
       this.players_vsTarget.classList.remove('invisible');
       this.players_vsTarget.classList.add('ball-hover');
-
-    } else if (event.currentTarget.innerHTML === 'Teams'){
+    } else if (event.currentTarget.innerHTML === 'Teams') {
       this.teamsTarget.classList.remove('invisible');
       this.teamsTarget.classList.add('ball-hover');
+    } else if (event.currentTarget.innerHTML === 'Fantasy League') {
+      this.fantasy_leagueTarget.classList.remove('invisible');
+      this.fantasy_leagueTarget.classList.add('ball-hover');
     }
   }
 
   mouseOut(event) {
-    if (event.currentTarget.innerHTML === 'Games') {
-      this.gamesTarget.classList.add('invisible');
-      this.gamesTarget.classList.remove('ball-hover');
-
-    } else if (event.currentTarget.innerHTML === 'Players'){
-      this.playersTarget.classList.add('invisible');
-      this.playersTarget.classList.remove('ball-hover');
-
+    if (event.currentTarget.innerHTML === 'Games reviews') {
+      this.games_reviewsTarget.classList.add('invisible');
+      this.games_reviewsTarget.classList.remove('ball-hover');
+    } else if (event.currentTarget.innerHTML === 'Players stats'){
+      this.players_statsTarget.classList.add('invisible');
+      this.players_statsTarget.classList.remove('ball-hover');
     } else if (event.currentTarget.innerHTML === 'Players VS'){
       this.players_vsTarget.classList.add('invisible');
       this.players_vsTarget.classList.remove('ball-hover');
-
     } else if (event.currentTarget.innerHTML === 'Teams'){
       this.teamsTarget.classList.add('invisible');
       this.teamsTarget.classList.remove('ball-hover');
+    } else if (event.currentTarget.innerHTML === 'Fantasy League') {
+      this.fantasy_leagueTarget.classList.add('invisible');
+      this.fantasy_leagueTarget.classList.remove('ball-hover');
     }
   }
 }
