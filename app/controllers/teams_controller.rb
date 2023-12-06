b78971c5287d87aad0b88ebe70b740c9744df9a5
CorentinDeseine <deseinecorentin@gmail.com>
2023-12-06 16:10:18 +0100

    fix: fix with good scale and time

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,7 +5,7 @@ class TeamsController < ApplicationController
 
   def show
     @team = Team.find(params[:id])
-    @past_games = @team.past_games(@team)
-    @players = @team.sort_players(@team, :rating).reject { |player| player.average(player.stats, player)[:min].zero? }
+    @past_games = @team.past_games
+    @players = @team.sort_players(:rating).reject { |player| player.average(player.stats, player)[:min].zero? }
   end
 end
